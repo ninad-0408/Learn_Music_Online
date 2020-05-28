b1fae904c24b5eacc3312549f740cb913a42c417
Ninad Kalanke <56078700+ninad-0408@users.noreply.github.com>
2020-05-28 21:50:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,59 @@
-const express=require('express');
-const app=express();
-const path=require('path');
-const dotenv=require('dotenv')
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
+app.use(express.urlencoded());
 
-app.use('/learnmusiconline.com',express.static('learnmusiconline.com'));
-
-app.set('view engine','pug');
-
-app.set('views',path.join(__dirname,'learnmusiconline.com/pug'));
+app.use('/learnmusiconline.com', express.static('learnmusiconline.com'));
 
 
-app.get('/',(req,res)=>{
+mongoose.connect('mongodb://localhost/learnmusiconline', { useNewUrlParser: true });
+
+app.set('view engine', 'pug');
+
+app.set('views', path.join(__dirname, 'learnmusiconline.com/pug'));
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("We are connecteed.../");
+});
+var kittySchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: Number,
+    username: String,
+    password: String,
+    cpassword: String
+});
+
+var store = mongoose.model('store', kittySchema);
+
+
+app.get('/', (req, res) => {
     res.status(200).render('home.pug');
 })
 
-app.get('/courses',(req,res)=>{
+app.get('/courses', (req, res) => {
     res.status(200).render('courses.pug');
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.status(404).render('notfound.pug');
 })
 
+app.post('/form', (req,res) => {
+    var fluffy = new store(req.body);
+    fluffy.save(function (err, fluffy) {
+        if (err) return console.error(err);
+        console.log(fluffy);
+    });
+    res.redirect('/');
+
+})
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
